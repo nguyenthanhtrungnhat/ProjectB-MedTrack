a76848a56a0192f1988a1e157b3abf7d779cab5d
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2025-02-12 10:50:20 +0700

    Update Screen.tsx

--- MedTrackWeb/src/Screen.tsx
+++ MedTrackWeb/src/Screen.tsx
@@ -119,6 +119,15 @@ export default function Screen() {
                                                     </div>
                                                 </div>
                                             </div>
+                                            <div className="col-4 marginBottom">
+                                                <div className="card room">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Room 001</h5>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                                        <h6 className='blueText text-center'>Emergency Department</h6>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
