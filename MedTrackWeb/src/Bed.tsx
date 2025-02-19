d66922c488d8ce008a3e468e9a854e7048071bb0
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2025-02-19 16:39:40 +0700

    Update Bed.tsx

--- MedTrackWeb/src/Bed.tsx
+++ MedTrackWeb/src/Bed.tsx
@@ -12,7 +12,7 @@ export default function Bed({fullName,patientID}:PatientProps) {
                 <button className="roomBtn"onClick={handleClick}>
                     <div className="card room">
                         <div className="card-body">
-                            <h5 className="card-title">Bed 001</h5>
+                            <h5 className="card-title">Bed {patientID}</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                             <h6 className='blueText text-center'>Patient's name: {fullName} - id: {patientID}</h6>
                         </div>
