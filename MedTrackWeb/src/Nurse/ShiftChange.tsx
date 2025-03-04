5eadd4bc7a6a78f7814f9ce7bae4037935ab2ca6
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2025-03-04 16:25:51 +0700

    Update ShiftChange.tsx

--- MedTrackWeb/src/Nurse/ShiftChange.tsx
+++ MedTrackWeb/src/Nurse/ShiftChange.tsx
@@ -71,7 +71,7 @@ export default function ShiftChange() {
                                         onClick={postData}
                                       
                                     >
-                                        
+                                        Submit
                                     </button>
                                 </div>
                             </form>
