25bc6161ebffea1d9f740162ccf65bc9b8ec649b
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2025-10-30 15:58:51 +0700

    Update CompleteData.tsx

--- MedTrackWeb/src/Patient/CompleteData.tsx
+++ MedTrackWeb/src/Patient/CompleteData.tsx
@@ -1,13 +1,21 @@
 export default function completeData() {
     return (
         <>
-            <div className="container">
+            <div className="container p-5 mt-5">
                 <div className="row">
                     <div className="col-lg-12">
                         <form className="row g-3">
                             <div className="col-md-6">
                                 <label htmlFor="inputEmail4" className="form-label">Email</label>
-                                <input type="email" className="form-control" id="inputEmail4"/>
+                                <input type="email" className="form-control" id="inputEmail4" />
+                            </div>
+                            <div className="col-md-6">
+                                <label htmlFor="inputPassword4" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="inputPassword4" />
+                            </div>
+                            <div className="col-12">
+                                <label htmlFor="inputAddress" className="form-label">Address</label>
+                                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
                             </div>
                         </form>
                     </div>
