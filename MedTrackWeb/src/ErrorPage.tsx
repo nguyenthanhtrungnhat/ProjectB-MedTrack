9b2e1466d1cb65043e0df22448f3e16179bc59d4
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2025-12-09 13:59:50 +0700

    Create ErrorPage.tsx

---
+++ MedTrackWeb/src/ErrorPage.tsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+    return (
+        <div className="alert alert-danger m-5 p-5">
+            <h5><strong>Note:</strong> Something went wrong. Please be patient and try again later.</h5>
+        </div>
+    )
+}
